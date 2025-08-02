b3430c057fa3213e93d99b632767763aa324fde3
yuuyuu661 <liuliliuli71@gmail.com>
2025-08-03 01:48:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+// server.js
 const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname)); // すべてのファイルを公開
+app.use(express.static(path.join(__dirname, 'public'))); // HTMLや画像のある場所
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
